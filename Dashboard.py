e065388df096a14dd639ae947f6b8b20ec5869a7
hzoscar <hz.oscar.ist@gmail.com>
2024-11-25 11:11:10 +0100

    Scripts del aula 2

--- Dashboard.py
+++ Dashboard.py
@@ -7,6 +7,18 @@ import warnings
 
 warnings.filterwarnings('ignore')
 
+
+## Funciones
+
+def formato_numero(valor,prefijo=''):
+    for unidad in ['','mil']:
+        if valor < 1000:
+            return f'{prefijo} {valor:.2f} {unidad}'
+        
+        valor /= 1000
+    
+    return f'{prefijo} {valor:.2f} millones'     
+
 st.title("DASHBOARD DE VENTAS :shopping_trolley:")
 
 url = 'https://ahcamachod.github.io/productos'
@@ -14,5 +26,13 @@ url = 'https://ahcamachod.github.io/productos'
 response = requests.get(url)
 soup = BeautifulSoup(response.content, 'html.parser')
 datos = pd.read_json(soup.pre.contents[0])
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.metric('Facturación', formato_numero(datos['Precio'].sum()),'COP')
+
+with col2:
+    st.metric('Cantidad de Ventas', formato_numero(datos.shape[0]))
 
 st.dataframe(datos)
